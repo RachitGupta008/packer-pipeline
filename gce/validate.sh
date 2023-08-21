0eb8438c1b5afc3e8afd3ba17b545ef42f90af11
RachitGupta008 <35948433+RachitGupta008@users.noreply.github.com>
2023-08-21 15:21:32 +0530

    Test

--- gce/validate.sh
+++ gce/validate.sh
@@ -2,14 +2,3 @@ echo "Hello World"
 ls -altr
 
 
-full_end_time=$(( $(date +%s) + 4200 ))
-while [ $(date +%s) -lt $full_end_time ]; do
-    current_time=$(date +%s)
-    end_time=$(( $current_time + 60 ))
-    echo "Printing this message every 1 minute."
-    echo "time $(date +%s) : $full_end_time"
-
-    while [ $(date +%s) -lt $end_time ]; do
-        a=21
-    done   
-done
